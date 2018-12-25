296f7dbc969a09e05ec968d7e10310879a8f630c
mariko-poyo <mariko.tatsumi@mail.utoronto.ca>
2018-12-25 22:51:45 +0000

    modified UI a lottle bit and make the server to be able to communicate with FPGA boards

---
+++ server/server/index.js
@@ -0,0 +1,38 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+const { fork } = require('child_process');
+
+app.get('/', function(req, res){
+ 	res.sendFile(__dirname + '/index.html');
+});
+
+//new connection
+io.on('connection', function(socket){
+	console.log('new connection established');
+
+	socket.on('add board', function(config){
+		var board_num = config.num;
+		console.log('add board with ip address of '+ config.IP);
+
+		//spawn child for querying to ip address continuously
+		const process = fork('./inter_client.js');
+		process.send(config);
+
+		//process data on reply from child proc
+		process.on('message', (data) => {
+			io.emit('temp val update', {board_num: board_num, temperatureval: data});
+		});
+	});
+});
+
+//send random value all the time
+setInterval(function() {
+  	var temperature_tmp = Math.floor(Math.random() * 1000);
+    io.emit('temp val update', {board_num: 1, temperatureval: temperature_tmp});
+}, 1000);
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
