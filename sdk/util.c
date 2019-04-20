af36453ffa4db37dfeed3c5b4b8d6a7bac4ed973
haiqi96 <haiqi96@gmail.com>
2019-04-20 10:15:52 +0900

    adding comment

--- sdk/util.c
+++ sdk/util.c
@@ -7,15 +7,17 @@ volatile char* MAC_SETTER = (char*)XPAR_SHELL_I_PACKET_FILTERING_SOURCE_MAC_ADDR
 //global driver
 XSysMon SysMonInst;
 
+
+
 int get_ADC_code(){
+    //Refer to page 15 of https://www.xilinx.com/support/documentation/ip_documentation/system_management_wiz/v1_3/pg185-system-management-wiz.pdf
 	int ADC_CODE = (*(int*)(SYSMON + 0x400)) >> 6;
 	return ADC_CODE;
 }
 
 u32 get_ADC_threshold(u32 decimal_threshold){
-	/* when you actually want to test the functionality
-	int temp_threshold = ceil((threshold + 273.6777) * 1024 / 501.3743);
-	*/
+    // The formula here is a reversed version of formula from page 40 of https://www.xilinx.com/support/documentation/user_guides/ug580-ultrascale-sysmon.pdf
+    // Using SYSMON1 and on chip reference
 	u32 ADC_threshold = ((decimal_threshold + 273.6777) * 1024 / 501.3743) + 1;
 	return ADC_threshold;
 }
@@ -30,17 +32,24 @@ void set_reset_threshold(u32 threshold){
 	AUTO_RESET_10BITS_mWriteReg(RESET_IP, 0x8, 1);
 }
 void set_src_macaddr(unsigned char src_mac_ethernet_address[], unsigned int debug_bit){
+    // convert first 4 bytes of mac address into a 32bit unsigned
 	unsigned int mac1 =
 			(src_mac_ethernet_address[0] << 24)
 			+ (src_mac_ethernet_address[1] << 16)
 			+ (src_mac_ethernet_address[2] << 8)
 			+ (src_mac_ethernet_address[3]);
+    // convert last 4 bytes of mac address into a 32bit unsigned
 	unsigned int mac2 =
 			(src_mac_ethernet_address[4] << 8)
 			+ (src_mac_ethernet_address[5]);
+            
+    // Write first 4 bytes of mac address into reg1
 	SOURCE_MAC_ADDRESS_SETTING_mWriteReg(MAC_SETTER, SOURCE_MAC_ADDRESS_SETTING_S00_AXI_SLV_REG1_OFFSET, mac1);
+    // Write last 2 bytes of mac address into reg2
 	SOURCE_MAC_ADDRESS_SETTING_mWriteReg(MAC_SETTER, SOURCE_MAC_ADDRESS_SETTING_S00_AXI_SLV_REG2_OFFSET, mac2);
+    // set debug_bit 
 	SOURCE_MAC_ADDRESS_SETTING_mWriteReg(MAC_SETTER, SOURCE_MAC_ADDRESS_SETTING_S00_AXI_SLV_REG3_OFFSET, debug_bit);
+    // Initialize the IP by writing 1 to reg0
 	SOURCE_MAC_ADDRESS_SETTING_mWriteReg(MAC_SETTER, SOURCE_MAC_ADDRESS_SETTING_S00_AXI_SLV_REG0_OFFSET, 0x1);
 }
 
@@ -54,6 +63,6 @@ void enable_sysmon_tempbus(){
 	}
 	XSysMon_CfgInitialize(&SysMonInst, sysmon_config, sysmon_config->BaseAddress);
 
-	// enable temp update from xadc
+	//call Xilinx provided function to enable temp update from xadc
 	XSysMon_EnableTempUpdate(&SysMonInst);
 }
--- sdk/util.h
+++ sdk/util.h
@@ -7,9 +7,26 @@
 //header file for sysmon interrupt functions
 #include "xsysmon.h"
 
+/* This file contains some wrapper function for IPs */
+
+// Get the SYSMON temperature value in ADC code format
 int get_ADC_code();
+
+// Take in a celsius Temperature and return the ADC code format temperature
 u32 get_ADC_threshold(u32 decimal_threshold);
+
+// Force board to reset directly
 void hard_reset();
+
+// Set the reset threshold. The input should be a integer celsius temperature
 void set_reset_threshold(u32 threshold);
+
+// Write the src mad address to custom Mac filter IP. 
+// If debug bit is 0, only packet with input src_mac_ethernet_address will be
+// sent to microblaze and other packets will go into user space
+// If debug bit is 1, all packets will go into shell space microblaze
 void set_src_macaddr(unsigned char src_mac_ethernet_address[], unsigned int debug_bit);
+
+// This function initialize the SYSMON and enable the 10bit temperature ADC code
+// to be output to a 10bit wire which connects to our custom reset IP
 void enable_sysmon_tempbus();
